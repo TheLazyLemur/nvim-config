3afb351e1960228b8fa3782bb83e83ebaf4fa48f
Dan <danrousseau@protonmail.com>
2022-02-07 09:55:09 +0200

    using plug instead of packer

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,74 +1,28 @@
 local fn = vim.fn
 
--- Automatically install packer
-local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system {
-    "git",
-    "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  }
-  print "Installing packer close and reopen Neovim..."
-  vim.cmd [[packadd packer.nvim]]
-end
-
--- Autocommand that reloads neovim whenever you save the plugins.lua file
 vim.cmd [[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
+call plug#begin()
+    Plug 'nvim-lua/popup.nvim'
+    Plug 'nvim-lua/plenary.nvim'
+    Plug 'nvim-lua/telescope.nvim'
+    Plug 'voldikss/vim-floaterm'
+    Plug 'preservim/nerdtree'
+    Plug 'airblade/vim-gitgutter'
+
+    Plug 'fatih/vim-go'
+    Plug 'neovim/nvim-lspconfig'
+    Plug 'github/copilot.vim'
+    Plug  'rust-lang/rust.vim'
+
+    Plug 'hrsh7th/nvim-cmp'
+    Plug 'hrsh7th/cmp-nvim-lsp'
+    Plug 'hrsh7th/cmp-buffer'
+    Plug 'hrsh7th/cmp-path'
+    
+    Plug 'mhinz/vim-startify'
+    Plug 'morhetz/gruvbox'
+call plug#end()
 ]]
 
--- Use a protected call so we don't error out on first use
-local status_ok, packer = pcall(require, "packer")
-if not status_ok then
-  return
-end
+vim.cmd [[ colorscheme gruvbox ]]
 
--- Have packer use a popup window
-packer.init {
-  display = {
-    open_fn = function()
-      return require("packer.util").float { border = "rounded" }
-    end,
-  },
-}
-
--- Install your plugins here
-return packer.startup(function(use)
-
-  use "wbthomason/packer.nvim" -- Have packer manage itself
-
-  -- Core Plugins
-  use "nvim-lua/popup.nvim"
-  use "nvim-lua/plenary.nvim" 
-  use "nvim-lua/telescope.nvim" 
-  use "voldikss/vim-floaterm"
-  use "preservim/nerdtree" 
-  use "airblade/vim-gitgutter"
-
-  -- Language Plugins
-  use "fatih/vim-go"
-  use 'neovim/nvim-lspconfig'
-  use "github/copilot.vim"
-  use  "rust-lang/rust.vim"
-
-  use "hrsh7th/nvim-cmp"
-  use "hrsh7th/cmp-nvim-lsp"
-  use "hrsh7th/cmp-buffer"
-  use "hrsh7th/cmp-path"
-
-  -- Appearance / Utility Plugins
-  use "dracula/vim"
-  use "mhinz/vim-startify"
-
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if PACKER_BOOTSTRAP then
-    require("packer").sync()
-  end
-end)
