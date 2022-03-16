7ed45c9809e504e3ed831583eaa14e701f98424e
Daniel Rousseau <danielro@vatit.com>
2022-03-16 16:26:55 +0200

    chore: turn keymaps into lua functions instead of vim strings

--- lua/user/lsp_installer.lua
+++ lua/user/lsp_installer.lua
@@ -13,10 +13,9 @@ local function lsp_keymaps()
     km("n", "<F2>", vim.lsp.buf.rename, {buffer=0})
     km("n", "<leader>dj", vim.diagnostic.goto_next, {buffer=0})
     km("n", "<leader>dk", vim.diagnostic.goto_prev, {buffer=0})
-    require('goto-preview').setup {}
-    km("n", "<leader>gpd", "<cmd>lua require('goto-preview').goto_preview_definition()<CR>")
-    km("n", "<leader>gP", "<cmd>lua require('goto-preview').close_all_win()<CR>")
-    km("n", "<leader>gpr", "<cmd>lua require('goto-preview').goto_preview_references()<CR>")
+    km("n", "<leader>gpd", require('goto-preview').goto_preview_definition)
+    km("n", "<leader>gP", require('goto-preview').close_all_win)
+    km("n", "<leader>gpr", require('goto-preview').goto_preview_references)
 end
 
 for _, name in pairs(servers) do
@@ -43,6 +42,7 @@ lsp_installer.on_server_ready(function(server)
 
     local opts = {
         on_attach = function ()
+            require('goto-preview').setup {}
             lsp_keymaps()
         end,
     }
--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -1,7 +1,34 @@
 local km = vim.keymap.set
 
+local telescope = require('telescope.builtin')
+
+local ignore_patterns = {
+    "venv",
+    "node_modules"
+}
+
+local function find_files()
+    telescope.find_files({
+        file_ignore_patterns = ignore_patterns
+    })
+end
+
+local function live_grep()
+    telescope.live_grep({
+        file_ignore_patterns = ignore_patterns
+    })
+end
+
+local function find_buffers()
+    telescope.buffers()
+end
+
+local function help_tags()
+    telescope.help_tags()
+end
+
 km("", "<Space>", "<Nop>")
-km("n", "<leader>f", "<cmd>lua require('telescope.builtin').find_files({file_ignore_patterns = { \"venv\", \"node_modules\" }})<CR>")
-km("n", "<leader>g", "<cmd>lua require('telescope.builtin').live_grep()<CR>")
-km("n", "<leader>b", "<cmd>lua require('telescope.builtin').buffers()<CR>")
-km("n", "<leader>h", "<cmd>lua require('telescope.builtin').help_tags()<CR>")
+km("n", "<leader>f", find_files)
+km("n", "<leader>g", live_grep)
+km("n", "<leader>b", find_buffers)
+km("n", "<leader>h", help_tags)
