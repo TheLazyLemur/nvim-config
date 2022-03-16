9c55eb63499ee85b4d0976f62140f795927080ee
Daniel Rousseau <danielro@vatit.com>
2022-03-16 14:32:11 +0200

    chore: fix order of plugins

--- init.lua
+++ init.lua
@@ -8,19 +8,27 @@ R = function (name)
 end
 
 RELOAD_CONFIG = function ()
-    R("user.plugins")
-    R("user.completion")
-    R("user.lsp")
-    R("user.treesitter")
     R("user.options")
     R("user.keymaps")
+    R("user.plugins")
+    R("user.colorscheme")
+    R("user.telescope")
+    R("user.nerdtree")
+    R("user.toggleterm")
+    R("user.treesitter")
+    R("user.completion")
+    R("user.lsp_installer")
     require("notify").notify("Neovim reloaded", "info")
 end
 
-require "user.plugins"
-require "user.treesitter"
 require "user.options"
 require "user.keymaps"
+require "user.plugins"
+require "user.colorscheme"
+require "user.telescope"
+require "user.nerdtree"
+require "user.toggleterm"
+require "user.treesitter"
 require "user.completion"
 require "user.lsp_installer"
 
