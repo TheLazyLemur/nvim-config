e2a00c701e18e94082d680d26b71551ddcddf02a
Dan Rousseau <danrousseau@protonmail.com>
2022-05-11 08:42:01 +0200

    fix(completion): fix completion keymaps

--- lua/sem-vim/completion.lua
+++ lua/sem-vim/completion.lua
@@ -7,7 +7,7 @@ ls.config.set_config {
     ext_opts = {
         [types.choiceNode] = {
             active = {
-                virt_text = {{"<--", "Error"}}
+                virt_text = { { "<--", "Error" } }
             }
         }
     }
@@ -17,20 +17,16 @@ local cmp = require('cmp')
 cmp.setup({
     snippet = {
         expand = function(args)
-             ls.lsp_expand(args.body)
+            ls.lsp_expand(args.body)
         end,
     },
-    mapping = {
-        ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-        ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-        ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-        ['<C-y>'] = cmp.config.disable,
-        ['<C-e>'] = cmp.mapping({
-            i = cmp.mapping.abort(),
-            c = cmp.mapping.close(),
-        }),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
-    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
     sources = cmp.config.sources({
         { name = 'nvim_lsp' },
         { name = 'luasnip' },
